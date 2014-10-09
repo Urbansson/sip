55e3f1be82b810382742530e986a3c5275339049
dolphan <emilvik@kth.se>
2014-10-09 13:42:13 +0200

    Tools for PDU handling

---
+++ src/sip/pdu/PDUHandler.java
@@ -0,0 +1,20 @@
+package sip.pdu;
+
+public class PDUHandler {
+	public static void handlePDU(PDU pdu){
+		switch(pdu){
+		case INVITE: 	break;
+		case TRYING:	break;
+		case RINGING:	break;
+		case OK:		break;
+		case ACK:		break;
+		case ALIVE:		break;
+		case BUSY:		break;
+		case ERROR:		break;
+		}
+	}
+	
+	void doStuff(){
+	}
+	
+}
---
+++ src/sip/pdu/Test.java
@@ -0,0 +1,9 @@
+package sip.pdu;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(PDUParser.parse("RINGING").getName());
+	}
+}
