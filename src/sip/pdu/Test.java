5844d3144baf8d4a2bc435139c0933afcd5d024f
dolphan <emilvik@kth.se>
2014-10-10 12:07:10 +0200

    Cleaned up project for new version

--- src/sip/pdu/Test.java
+++
@@ -1,9 +0,0 @@
-package sip.pdu;
-
-public class Test {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println(PDUParser.parse("RINGING").getName());
-	}
-}
